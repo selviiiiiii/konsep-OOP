b7c3159c42df5d65a2899e287324c57e7b89c33d
HP <lestariselvi27@gmail.com>
2024-09-27 11:54:52 +0700

    add.AccessModifierA.kt

---
+++ src/main/kotlin/id/ac/polbeng/selvi/test_kelas/AccessModifierA.kt
@@ -0,0 +1,39 @@
+package id.ac.polbeng.selvi.test_kelas
+
+open class Mahasiswa(var nama: String, var nrp: Int){
+    open fun info() {
+        println(nama +'\n'+ nrp)
+    }
+    override fun toString(): String {
+        return "Mahasiswa{nama= $nama, nrp= $nrp}"
+    }
+}
+
+class KetuaHima(nama: String, nrp: Int, val jurusan: String) : Mahasiswa(nama, nrp) {
+    override fun info(){
+        println(nama +'\n'+ nrp +'\n'+ jurusan)
+    }
+}
+
+fun main(){
+    val anton = KetuaHima("Anton", 1106789, "Teknik Informatika")
+    val budi = Mahasiswa("Budi Gunawan", 1106123)
+
+
+    println(anton.toString())
+    println()
+    anton.info()
+    println("Jenis Kelas = " + anton.javaClass.simpleName)
+
+    println("\n")
+
+
+    budi.info()
+    println("Jenis Kelas = " + budi.javaClass.simpleName)
+
+
+    val ucok = KetuaHima("Ucok", 1105239, "Teknik Elektro")
+    println("\n")
+    ucok.info()
+    println("Jenis Kelas = " + ucok.javaClass.simpleName)
+}
